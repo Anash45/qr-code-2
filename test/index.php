5a2505e3b4a1f01292ae305123d6c226b1103660
Syed Anas Bukhari <anas14529@gmail.com>
2024-05-25 06:20:21 +0500

    Test 4

--- test/index.php
+++ test/index.php
@@ -8,7 +8,7 @@
 <body>
     <h1>Join Our WiFi Network</h1>
     <p>Scan the QR code below to join the network:</p>
-    <img src="https://api.qrserver.com/v1/create-qr-code/?data=https%3A%2F%2Fportfolio.f4futuretech.com%2Fqr-code-2%2Ftrack.php%3Fssid%3DYOUR_SSID%26password%3DYOUR_PASSWORD&size=200x200" alt="WiFi QR Code">
+    <img src="https://api.qrserver.com/v1/create-qr-code/?data=https%3A%2F%2Fportfolio.f4futuretech.com%2Fqr-code-2%2Ftest%2Ftrack.php%3Fssid%3DYOUR_SSID%26password%3DYOUR_PASSWORD&size=200x200" alt="WiFi QR Code">
 
     <p>Network SSID: YOUR_SSID</p>
     <p>Password: YOUR_PASSWORD</p>
